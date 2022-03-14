54a6f79de87bd94974d1695b85c0651e06af9141
Hallelujah Kebede <matiashalleluya19@gmail.com>
2022-03-14 14:24:38 +0000

    Parse animal names and bodyparts

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,13 +1,118 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * 
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    /**
+     * 
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+        String filename = in.nextLine();
+        in.close();
+
+        // Check format
+        if (!filename.endsWith(".csv")) {
+            throw new IllegalArgumentException(filename);
+        }
+
+        // Get cumulative data output file
+        cumulativeData(filename);
+
     }
+
+    /**
+     * 
+     * @param filename
+     * @throws IOException
+     */
+
+    public static void cumulativeData(String filename) throws IOException {
+
+        Parser parser = new Parser(filename);
+        Parser.parse(filename);
+
+        
+
+        // System.out.println(filename);
+        // ArrayList<Integer> sortedOrigin = new
+        // ArrayList<Integer>(parser.getOriginYears());
+        // ArrayList<Integer> sortedDevelopment = new
+        // ArrayList<Integer>(parser.getDevelopmentYears());
+        // ArrayList<String> productList = new ArrayList<String>(parser.getProducts());
+
+        // Collections.sort(sortedOrigin);
+        // Collections.sort(sortedDevelopment);
+
+        // // Output String instantiation
+        // StringBuilder output = new StringBuilder();
+
+        // // Construct first line of output
+        // // System.out.println(parser.getOriginYears() + ", " +
+        // // sortedDevelopment.size());
+        // output.append(sortedOrigin.get(0) + ", " + sortedDevelopment.size() + "\n");
+
+        // // Construct the output by line per product
+        // for (int i = 0; i < parser.getProducts().size(); i++) {
+        // String product = productList.get(i);
+
+        // // No of development years = last development year - earliest origin year
+        // // No of origin years = final origin year - earliest origin year
+        // int originYear = sortedOrigin.get(0);
+        // int finalYear = sortedOrigin.get(sortedOrigin.size() - 1);
+        // int n = sortedDevelopment.get(sortedDevelopment.size() - 1) - originYear + 1;
+
+        // System.out.println(parser.getAllProducts().get(product).ProductToString(originYear,
+        // finalYear, n));
+        // if (i == parser.getProducts().size() - 1) {
+        // output.append(parser.getAllProducts().get(product).ProductToString(originYear,
+        // finalYear, n));
+        // } else {
+        // output.append(parser.getAllProducts().get(product).ProductToString(originYear,
+        // finalYear, n) + "\n");
+        // }
+
+        // }
+
+        // writeToFile(output.toString());
+    }
+
+    /**
+     * 
+     * @param output
+     */
+    public static void writeToFile(String output) {
+
+        File file = new File("Output.csv");
+        FileWriter fileWriter = null;
+
+        try {
+            fileWriter = new FileWriter(file);
+            fileWriter.write(output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
---
+++ demo/src/main/java/com/example/Parser.java
@@ -0,0 +1,207 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Parser {
+
+    public String name;
+    public static Set<String> products = new HashSet<>();
+    public static Set<String> animals = new HashSet<>();
+    public static Set<String> parts = new HashSet<>();
+    public static Set<Integer> originYears = new HashSet<>();
+    public static Set<Integer> developmentYears = new HashSet<>();
+    public static HashMap<String, Product> allProducts = new HashMap<String, Product>();
+
+    public Parser(String filename) {
+        this.name = filename;
+    }
+
+    /**
+     * 
+     * @param filename
+     * @throws IOException
+     */
+    public static void parse(String filename) throws IOException {
+
+        ArrayList<String> content = (ArrayList<String>) Files
+                .readAllLines(Paths.get(System.getProperty("user.dir") + File.separator + filename));
+
+        HashSet<String> nameRow = getAnimals(content.get(1));
+        HashSet<String> partRow = getParts(content.get(2));
+
+        System.out.println(animals);
+        System.out.println(parts);
+
+        // for (int j = 1; j < content.size(); j++) {
+
+        // // Get every row of the file
+        // String[] row = content.get(j).replaceAll("\"", " ").split(",");
+
+        // if (row == null) {
+        // System.out.println("Error message: " + "Empty row, Value Ignored in line" +
+        // j);
+        // continue;
+        // }
+
+        // // Check validity of origin or development year
+        // if (row[1] == null || row[2] == null) {
+        // System.out.println("Error message: " + "Invalid value of Origin year [" +
+        // row[1] + "] or development year ["
+        // + row[2] + "], Value Ignored in line " + j);
+        // continue;
+        // }
+
+        // int originYear;
+        // int developmentYear;
+
+        // try {
+        // originYear = Integer.valueOf(row[1].trim());
+        // developmentYear = Integer.valueOf(row[2].trim());
+        // } catch (NumberFormatException e) {
+        // // handle exception
+        // System.out.println("Error message: " + "Invalid types of Origin year [" +
+        // row[1] + "] or development year ["
+        // + row[2] + "], Values Ignored in line " + j);
+        // continue;
+        // }
+
+        // // Check validity of origin or development year
+        // if (originYear < 0 || developmentYear < 0) {
+        // System.out.println("Error message: " + "Invalid value of Origin year [" +
+        // row[1] + "] or development year ["
+        // + row[2] + "], Value Ignored");
+        // continue;
+        // }
+
+        // // Check if valid combination of origin and development years
+        // if (originYear > developmentYear) {
+        // System.out.println("Error message: " + "Origin Year [" + row[1] +
+        // "] greater than Development Year ["
+        // + row[2] + "] Value Ignored in line " + j);
+        // continue;
+        // }
+
+        // ArrayList<Integer> keys = new ArrayList<Integer>();
+        // keys.add(originYear);
+        // keys.add(developmentYear);
+
+        // double payment;
+        // try {
+        // payment = Double.valueOf(row[3]);
+        // } catch (NumberFormatException e) {
+        // // handle exception
+        // System.out.println("Error message: " + "Invalid types of Origin year [" +
+        // row[1] + "] or development year ["
+        // + row[2] + "], Values Ignored in line" + j);
+        // continue;
+
+        // }
+
+        // // Update the origin and development year lists
+        // originYears.add(originYear);
+        // developmentYears.add(developmentYear);
+
+        // makeProduct(row[0], keys, payment);
+
+        // }
+
+    }
+
+    /**
+     * 
+     * @param productName
+     * @param yearPairs
+     * @param payment
+     */
+
+    private static void makeProduct(String productName, ArrayList<Integer> yearPairs, double payment) {
+
+        // Create product objects or update their records
+        if (!products.contains(productName)) {
+            Product product = new Product(productName);
+            allProducts.put(productName, product);
+            product.getRecords().put(yearPairs, payment);
+            products.add(productName);
+        } else {
+            allProducts.get(productName).getRecords().put(yearPairs, payment);
+            products.add(productName);
+        }
+
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Set<String> getProducts() {
+        return products;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public HashMap<String, Product> getAllProducts() {
+        return allProducts;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Set<Integer> getOriginYears() {
+        return originYears;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Set<Integer> getDevelopmentYears() {
+        return developmentYears;
+    }
+
+    /**
+     * 
+     * @param line
+     * @return
+     */
+    private static HashSet<String> getAnimals(String line) {
+
+        String[] namesRow = line.replaceAll("\"", " ").split(",");
+        List<String> list = Arrays.asList(namesRow);
+        HashSet<String> nameSet = new HashSet<>();
+        nameSet.addAll(list);
+
+        nameSet.remove("individuals");
+        animals = nameSet;
+        return nameSet;
+    }
+
+    /**
+     * 
+     * @param line
+     * @return
+     */
+    private static HashSet<String> getParts(String line) {
+
+        String[] partsRow = line.replaceAll("\"", " ").split(",");
+        List<String> list = Arrays.asList(partsRow);
+        HashSet<String> partSet = new HashSet<>();
+        partSet.addAll(list);
+
+        partSet.remove("bodyparts");
+        parts = partSet;
+        return partSet;
+    }
+
+}
